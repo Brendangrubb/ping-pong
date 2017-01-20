fa354529de4aa8e69b32c989cacf57e9bca013a8
Brendan Grubb <brendangrubb@Brendans-MacBook-Pro.local>
2017-01-20 13:06:31 -0800

    add alert of ping, pong & pingpong for 3, 5 & 15 entered in input field

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
+//Business Logic
+
+
 
 // User Interface Logic
+
 $(document).ready(function() {
+
   $("#input-form").submit(function(event) {
     event.preventDefault();
-    var userInput = $("#enter-number").val();
-    $("ul#number-output").prepend("<li>" + userInput + "</li>");
+
+    var numberInput = parseInt($("#enter-number").val());
+
+    if (numberInput % 3 === 0 && numberInput % 5 !== 0) {
+      alert("ping");
+    } else if (numberInput % 5 === 0 && numberInput % 15 !== 0) {
+      alert("pong");
+    } else if (numberInput % 15 === 0) {
+      alert("pingpong");
+    }
+
+    // $("ul#number-output").prepend("<li>" + numberInput + "</li>");
+
 
   })
+
 
 })
