9414a1b0d0c2f1706933e5ea325c304fae148124
Brendan Grubb <brendangrubb@Brendans-MacBook-Pro.local>
2017-01-20 16:41:53 -0800

    combine logic to make program operational

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,3 @@
-var forUser = fucntion(numberInput) {
-
-  for (index = 1; index <= numberInput; index += 1) {
-  result = index;
-
-    if (index % 3 === 0 && index % 5 !== 0) {
-      result = "ping";
-    } else if (index % 5 === 0 && index % 15 !== 0) {
-      result = "pong";
-    } else if (index % 15 === 0 && index % 150 !== 0) {
-      result = "pingpong";
-    } else if (index % 150 === 0) {
-      result = "pingpongdingdong"
-    };
-
-  };
-};
-
-
 $(document).ready(function() {
 
   $("#input-form").submit(function(event) {
@@ -25,8 +6,22 @@ $(document).ready(function() {
 
     var numberInput = parseInt($("#enter-number").val());
 
-    $("ul#number-output").append("<li>" + forUser + "</li>");
+    for (index = 1; index <= numberInput; index += 1) {
+      result = index;
 
+      if (index % 3 === 0 && index % 5 !== 0) {
+        result = "ping";
+      } else if (index % 5 === 0 && index % 15 !== 0) {
+        result = "pong";
+      } else if (index % 15 === 0 && index % 150 !== 0) {
+        result = "pingpong";
+      } else if (index % 150 === 0) {
+        result = "pingpongdingdong"
+      };
+
+      $("ul#number-output").append("<li>" + result + "</li>");
+
+    };
 
  });
 
