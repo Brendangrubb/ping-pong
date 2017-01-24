669cd3e9ece2a954086a40686c57c78b28344e7f
Brendan Grubb <brendangrubb@Brendans-MacBook-Pro.local>
2017-01-23 20:35:51 -0800

    create section for business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,32 @@
 // Business Logic
 
-for (index = 1; index <= userInput; index += 1) {
-  result = index;
 
-  if (index % 3 === 0 && index % 5 !== 0) {
-    result = "ping";
-  } else if (index % 5 === 0 && index % 15 !== 0) {
-    result = "pong";
-  } else if (index % 15 === 0) {
-    result = "pingpong";
-  };
 
-};
+
 
 
 // User Interface Logic
 $(document).ready(function() {
 
-  var userInput = parseInt($("#user-enter").val());
-
   $("#input-form").submit(function(event) {
     event.preventDefault();
     $("ul#user-output").empty();
 
+    var userInput = parseInt($("#user-enter").val());
 
-    $("ul#user-output").append("<li>" + result + "</li>");
+    for (index = 1; index <= userInput; index += 1) {
+      result = index;
+
+      if (index % 3 === 0 && index % 5 !== 0) {
+        result = "ping";
+      } else if (index % 5 === 0 && index % 15 !== 0) {
+        result = "pong";
+      } else if (index % 15 === 0) {
+        result = "pingpong";
+      };
+
+      $("ul#user-output").append("<li>" + result + "</li>");
+    };
 
  });
 
