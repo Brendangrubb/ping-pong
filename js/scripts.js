4e82a6bf2427b2df5288d68d0f0b13beb64f74c2
Brendan Grubb <brendangrubb@gmail.com>
2017-01-29 14:13:42 -0800

    show array of index up to userEnter in ul

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,33 @@
 // Business Logic
 
 var userEnter;
-
+var result;
+var userOutput = [];
 
 var processUserEnter = function(userEnter) {
 
   parseInt(userEnter);
   // return userEnter;
   for (i = 1; i <= userEnter; i += 1) {
-    alert(i);
-
+    result = i;
+    if (i % 3 === 0 && i % 5 !== 0) {
+      result = "ping";
+    } else if (i % 5 === 0 && i % 15 !== 0) {
+      result = "pong";
+    } else if (i % 15 === 0) {
+      result = "pingpong";
+    };
+    userOutput.push(result);
 
   };
 
+  console.log(userOutput);
+
+
 };
 
-//   // if (index % 3 === 0 && index % 5 !== 0) {
-//   //   result = "ping";
-//   // } else if (index % 5 === 0 && index % 15 !== 0) {
-//   //   result = "pong";
-//   // } else if (index % 15 === 0) {
-//   //   result = "pingpong";
-//   // };
+
+
 //
 
 
@@ -31,11 +37,13 @@ $(document).ready(function() {
 
   $("#input-form form").submit(function() {
     event.preventDefault();
+    $("ul#user-output").empty();
+
 
     var userEnter = $("#user-enter").val();
     var output = processUserEnter(userEnter);
 
-    $("ul#user-output").append("<li>" + output + "</li>");
+    $("ul#user-output").prepend("<li>" + userOutput + "</li>");
   })
 
 });
