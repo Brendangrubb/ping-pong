6d10f0c947f640091cd31a9c69d17ba742d15302
Brendan Grubb <brendangrubb@gmail.com>
2017-01-29 12:39:20 -0800

    begin to separate business and UI logic by establishing variables anda  function in business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,109 @@
 // Business Logic
 
+var userEnter;
 
+
+var processUserEnter = function(userEnter) {
+
+  parseInt(userEnter);
+  return userEnter * 2;
+}
 
 
 
 
 // User Interface Logic
+
 $(document).ready(function() {
 
-  $("#input-form").submit(function(event) {
+
+
+  $("#input-form form").submit(function() {
     event.preventDefault();
-    $("ul#user-output").empty();
 
-    var userInput = parseInt($("#user-enter").val());
+    var userEnter = $("#user-enter").val();
+    var result = processUserEnter(userEnter);
 
-    for (index = 1; index <= userInput; index += 1) {
-      result = index;
+    $("ul#user-output").append("<li>" + result + "</li>");
+  })
 
-      if (index % 3 === 0 && index % 5 !== 0) {
-        result = "ping";
-      } else if (index % 5 === 0 && index % 15 !== 0) {
-        result = "pong";
-      } else if (index % 15 === 0) {
-        result = "pingpong";
-      };
 
-      $("ul#user-output").append("<li>" + result + "</li>");
-    };
 
- });
+
+
+
+
 
 });
+
+
+
+
+// var userEnter = $("#user-enter").val();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Unseparated Business & UI Logic
+
+// $(document).ready(function() {
+//
+//   $("#input-form").submit(function(event) {
+//     event.preventDefault();
+//     $("ul#user-output").empty();
+//
+//     var userInput = parseInt($("#user-enter").val());
+//
+//     for (index = 1; index <= userInput; index += 1) {
+//       result = index;
+//
+//       if (index % 3 === 0 && index % 5 !== 0) {
+//         result = "ping";
+//       } else if (index % 5 === 0 && index % 15 !== 0) {
+//         result = "pong";
+//       } else if (index % 15 === 0) {
+//         result = "pingpong";
+//       };
+//
+//       $("ul#user-output").append("<li>" + result + "</li>");
+//     };
+//
+//  });
+//
+// });
